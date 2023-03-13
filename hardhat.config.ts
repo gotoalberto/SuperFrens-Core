4e7b1826a24a75b5693d460927181ecd69bb606a
Alvaro Teran Rodriguez <ukhezo.web3@gmail.com>
2023-03-14 00:37:05 +0100

    feat:Add mumbai hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MUMBAI_RPC_URL: string = process.env.MUMBAI_RPC_URL as string;
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80001,
+    },
+  },
   solidity: "0.8.14",
 };
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const FlowSenderFactory = await ethers.getContractFactory(
+    "FlowSenderFactory"
   );
+  const flowSenderFactory = await FlowSenderFactory.deploy();
+
+  await flowSenderFactory.deployed();
+
+  console.log(`flowSenderFactory deployed to ${flowSenderFactory.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => (process.exitCode = 0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });

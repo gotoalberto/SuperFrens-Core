fb125ba7bbce3c2b13af50576f87c5cb33e31bdb
0xukhezo <ukhezo.web3@gmail.com>
2023-03-23 17:28:25 +0100

    feat:Add fee to open a campaign & delete hardcoding and commits

--- contracts/FlowSenderfactory.sol
+++ contracts/FlowSenderfactory.sol
@@ -1,31 +1,66 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity 0.8.14;
 
-import {FlowSender} from "./FlowSender.sol";
+import {ISuperfluid, ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import { ISuperfluid, ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+contract FlowSender {
+    using CFAv1Library for CFAv1Library.InitData;
+    CFAv1Library.InitData public cfaV1;
 
-
-contract FlowSenderFactory {
-    mapping (address => FlowSender) public accountList;
-
-    event ContractCreated(address flowSenderAddress);
-
-    ISuperToken public fUSDCx = ISuperToken(0x42bb40bF79730451B11f6De1CbA222F17b87Afd7);
-    ISuperfluid public hostPolygon = ISuperfluid(0xEB796bdb90fFA0f28255275e16936D25d3418603);
-
+    mapping(address => bool) public accountList;
+    ISuperToken public tokenX;
+    int96 public amountFlowRate;
     address public owner;
-    
-     constructor(){   
-        owner = msg.sender;
+
+    constructor(
+        ISuperfluid _host,
+        ISuperToken _tokenX,
+        int96 _amountFlowRate,
+        address _owner
+    ) {
+        cfaV1 = CFAv1Library.InitData(
+            _host,
+            IConstantFlowAgreementV1(
+                address(
+                    _host.getAgreementClass(
+                        keccak256(
+                            "org.superfluid-finance.agreements.ConstantFlowAgreement.v1"
+                        )
+                    )
+                )
+            )
+        );
+        owner = _owner;
+        tokenX = _tokenX;
+        amountFlowRate = _amountFlowRate;
     }
 
-    function deployFlowSender(address _client, int96 _amountFlowRate) external returns(address flowSenderAddress){
-        FlowSender newFlowSender = new FlowSender(hostPolygon, fUSDCx, _amountFlowRate);
+    function gainTokenX(uint256 amount) external {
+        ERC20 erc20 = ERC20(tokenX.getUnderlyingToken());
+        erc20.transferFrom(msg.sender, address(this), amount);
 
-        accountList[_client] = newFlowSender;
-        flowSenderAddress = address(newFlowSender);
+        uint256 fee = (amount * 300) / 10000;
+        erc20.transfer(owner, fee);
 
-        emit ContractCreated(flowSenderAddress);
+        erc20.approve(address(tokenX), 2 * (amount - fee));
+        tokenX.upgrade(amount - fee);
+    }
+
+    function createStream(address receiver) external {
+        int96 flowRate = amountFlowRate / 2592000;
+        cfaV1.createFlow(receiver, tokenX, flowRate);
+        accountList[receiver] = true;
+    }
+
+    function deleteStream(address receiver) external {
+        cfaV1.deleteFlow(address(this), receiver, tokenX);
+        accountList[receiver] = false;
+    }
+
+    function updateStream(int96 flowRate, address receiver) external {
+        cfaV1.updateFlow(receiver, tokenX, flowRate);
     }
 }
